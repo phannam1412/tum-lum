3804c7ebd3fe23203ab432f46a030ec9b0ca3755
phannam1412 <phannam1412@yahoo.com>
2019-12-05 14:51:32 +0700

    Create json-to-csv.php

---
+++ json-to-csv.php
@@ -0,0 +1,21 @@
+<?php
+
+global $argv;
+if(count($argv) < 2)
+    throw new Exception('please specify the directory that you want to combine all your *.json file');
+$files = glob($argv[1] . '/test*.json');
+print_r($files);
+if(empty($files))
+    throw new Exception('no json file detected');
+$headers = [];
+$json = json_decode(file_get_contents($files[0]), true);
+$headers = array_keys($json);
+$output = [];
+unlink('output.csv');
+$f = fopen('output.csv', 'w');
+fputcsv($f, $headers);
+foreach($files as $file) {
+    $json = json_decode(file_get_contents($file), true);
+    fputcsv($f, $json);
+}
+fclose($f);
