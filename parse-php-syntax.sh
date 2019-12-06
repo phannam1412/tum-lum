26335f3368f92d6b5e71de0a14d00afa7a949852
phannam1412 <phannam1412@yahoo.com>
2019-12-06 10:58:41 +0700

    Update parse-php-syntax.sh

--- parse-php-syntax.sh
+++ parse-php-syntax.sh
@@ -4,6 +4,6 @@ mkdir /go/src/app
 cd /go/src/app
 GOPATH=/go/src/app
 go get -u github.com/phannam1412/go-pattern-matching
-rm parse-php-syntax.go
-wget https://raw.githubusercontent.com/phannam1412/tum-lum/master/parse-php-syntax.go
-go run parse-php-syntax.go
+rm parse-mysql-syntax.go
+wget https://raw.githubusercontent.com/phannam1412/tum-lum/master/parse-mysql-syntax.go
+go run parse-mysql-syntax.go
