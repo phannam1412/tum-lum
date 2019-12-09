a76c7cdae098df631628fcac5f4652e121938ef6
phannam1412 <phannam1412@yahoo.com>
2019-12-09 17:32:10 +0700

    Update sunflower.sh

--- sunflower.sh
+++ sunflower.sh
@@ -4,4 +4,4 @@ sdkmanager "platform-tools" "platforms;android-28" "build-tools;28.0.0"
 git clone https://github.com/android/sunflower.git
 cd sunflower
 ./gradlew assembleDebug --stacktrace
-echo generated apk at ./app/build/outputs/apk/app-debug.apk
+echo generated apk at ./app/build/outputs/apk/debug/app-debug.apk
